1548cf67ec448e89a36250f8e650011bb8e1576f
Hemanth Veera <148690629+HemanthGit01@users.noreply.github.com>
2024-12-01 21:54:56 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -3,7 +3,11 @@ package com.driver;
 public class Main {
     public static void main(String[] args) {
         F1 mercedes = new F1("mercedes", false);
+
+        // Test move
         mercedes.move(40, 0);
+
+        // Test accelerate
         mercedes.accelerate(20);
         mercedes.accelerate(-60);
     }
