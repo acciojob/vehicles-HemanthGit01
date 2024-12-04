77954da3ac6a6580d53c19d2d58ef95f8a4287db
Hemanth Veera <148690629+HemanthGit01@users.noreply.github.com>
2024-12-04 19:54:28 +0530

    Update Car.java

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -2,31 +2,71 @@ package com.driver;
 
 public class Car extends Vehicle {
     private int wheels;
-    private String type;
     private int doors;
     private int gears;
     private boolean isManual;
     private int currentGear;
     private int seats;
+    private String type;
 
     public Car(String name, int wheels, int doors, int gears, boolean isManual, String type, int seats) {
-        super(name); // Call Vehicle constructor
+        super(name);
         this.wheels = wheels;
         this.doors = doors;
         this.gears = gears;
         this.isManual = isManual;
-        this.type = type;
+        this.currentGear = 1;  // Default gear
         this.seats = seats;
-        this.currentGear = 1; // Default gear
+        this.type = type;
+    }
+     public int getGears() {
+        return this.gears;
     }
 
-    public void changeGear(int newGear) {
-        this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+     public void changeGear(int gear) {
+        this.currentGear = gear;
+        System.out.println("Car.setCurrentGear(): Changed to " + this.currentGear + " gear.");
     }
 
-    public void changeSpeed(int newSpeed, int newDirection) {
-        super.move(newSpeed, newDirection); // Call move method of Vehicle
-        System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+ public void changeSpeed(int speed, int direction) {
+    int gear;
+    if (speed > 0 && speed <= 50) {
+        gear = 1;
+    } else if (speed <= 100) {
+        gear = 2;
+    } else if (speed <= 150) {
+        gear = 3;
+    } else if (speed <= 200) {
+        gear = 4;
+    } else if (speed <= 250) {
+        gear = 5;
+    } else {
+        gear = 6;
+    }
+    changeGear(gear);
+    super.move(speed, direction);
+}
+    public int getWheels() {
+        return wheels;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public int getSeats() {
+        return seats;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public boolean isManual() {
+        return isManual;
+    }
+
+    public int getCurrentGear() {
+        return currentGear;
     }
 }
